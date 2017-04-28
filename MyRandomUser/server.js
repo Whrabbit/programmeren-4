4c47f4377c0a576e796800ecbbbec83a53e4e636
Whrabbit <whitneyrabbitc@gmail.com>
2017-04-28 14:52:33 +0200

    Initial commit

--- MyRandomUser/server.js
+++ MyRandomUser/server.js
@@ -1,25 +1,25 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function(request, response) {
-    response.send('Hello Avans!');
-})
-
-app.get('/about', function(request, response) {
-    response.send('Written by whitney');
-})
-
-app.post('/', function(request, response) {
-    response.send('Hello Avans, POST request received!');
-})
-
-app.put('/', function(request, response) {
-    response.send('Hello Avans, PUT request received!');
-})
-
-app.listen(3000, function() {
-    console.log('Server app is listening on port 3000');
-})
+// var express = require('express');
+// var app = express();
+//
+// app.get('/', function(request, response) {
+//     response.send('Hello Avans!');
+// })
+//
+// app.get('/about', function(request, response) {
+//     response.send('Written by whitney');
+// })
+//
+// app.post('/', function(request, response) {
+//     response.send('Hello Avans, POST request received!');
+// })
+//
+// app.put('/', function(request, response) {
+//     response.send('Hello Avans, PUT request received!');
+// })
+//
+// app.listen(3000, function() {
+//     console.log('Server app is listening on port 3000');
+// })
 
 // var express = require('express');
 // var app = express();
@@ -36,3 +36,28 @@ app.listen(3000, function() {
 // app.listen(3000, function() {
 //     console.log('Server app is listening on port 3000');
 // })
+
+app.get('/json', function(request, response) {
+    response.json({
+        'some_name': 'Value',
+        'an_array_of_objects': [
+            {
+                'another_name': 'Another value',
+                'a_further_name': "A further value"
+            },
+            {'yet_another_name': 'Yet another value'}
+        ],
+        'some_boolean': true,
+        'some_integer': 42,
+        'array_of_ints': [
+            2, 3, 5, 7, 11, 13
+        ],
+        'array_of_strings': [
+            "twee", "drie", "vijf", "zeven"
+        ]
+    })
+})
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+    console.log('Server app is listening on port ' + port);
+})
