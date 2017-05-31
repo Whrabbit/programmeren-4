a64b7fc6ed92bdcecfe9fc370c1442477b86cacc
Whrabbit <whitneyrabbitc@gmail.com>
2017-05-31 22:29:02 +0200

    world

--- world/routes/routes_apiv1.js
+++ world/routes/routes_apiv1.js
@@ -1,15 +1,55 @@
 var express = require('express');
 var router = express.Router();
-var connector = require('../db/db_connector');
-var pool = require('../db/db_connector');
+var moment = require('moment');
+var jwt = require('jwt-simple');
+var bcrypt = require('bcrypt');
 
-router.get('/login', function (req, res, next) {
-    var username = req.query.username || '';
-    var password = req.query.password || '';
+var bodyParser = require('body-parser');
+router.use(bodyParser.json()); // support json encoded bodies
+router.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+var path = require('path');
+var pool = require('../db/db_connector');
+var config = require('../config.json');
+
+function encodeToken(username) {
+    const payload = {
+        exp: moment().add(10, 'days').unix(),
+        iat: moment().unix(),
+        sub: username
+    };
+    return jwt.encode(payload, config.secretkey);
+}
+
+function decodeToken(token, cb) {
+
+    try {
+        const payload = jwt.decode(token, config.secretkey);
+
+        //check if token has expired
+        const now = moment().unix();
+
+        if (now > payload.exp) {
+            console.log('Token has expired.')
+        }
+
+        //callback
+        cb(null, payload);
+
+    } catch (err) {
+        //callback
+        cb(err, null);
+    }
+}
+
+router.post('/login', function(req, res, next) {
+
+    var username = req.body.username || '';
+    var password = req.body.password || '';
 
     if (username && password) {
-        var query_str = 'SELECT * FROM users WHERE username = "' + username + '" AND password = "' + password + '";';
-        console.log(query_str);
+        query_str = 'SELECT * FROM users WHERE username = "' + username + '";';
+
         pool.getConnection(function (err, connection) {
             if (err) {
                 throw err
@@ -19,21 +59,36 @@ router.get('/login', function (req, res, next) {
                 if (err) {
                     throw err
                 }
-                res.status(200).json(rows);
-                if(rows.hasOwnProperty("username", username)){
-                    console.log("got em");
+
+                if (rows[0]) {
+                    if (rows[0].hasOwnProperty('username') && rows[0].hasOwnProperty('password')) {
+                        var hash = rows[0].password;
+                        console.log(hash);
+                        console.log(bcrypt.compareSync(password, hash));
+                        if (bcrypt.compareSync(password, hash)){
+                            res.status(200).json(encodeToken(username));
+                        } else {
+                            res.json({message:"Invalid password"});
+                        }
+                    } else {
+                        res.json({error: "Please enter a valid username and password"});
+                    }
+                } else {
+                    res.json({error: "Please enter a valid username and password"});
                 }
+
             });
         });
     } else {
-       res.json({message: "Please provide a valid username and password"});
+        res.json({error: "Please enter a valid username and password"});
     }
+
 });
 
 router.all(new RegExp("[^\/login)]"), function (req, res, next) {
-    var token = (req.header('X-Acces-Token')) || '';
+    var token = (req.header('X-Access-Token')) || '';
 
-    auth.decodeToken(token, function (err, payload) {
+    decodeToken(token, function (err, payload) {
         if (err) {
             console.log('Error handler: ' + err.message);
             res.status((err.status || 401 )).json({error: new Error("Not authorised").message});
@@ -41,6 +96,33 @@ router.all(new RegExp("[^\/login)]"), function (req, res, next) {
             next();
         }
     });
+});
+
+router.post('/register', function(req, res, next){
+    var username = req.body.username;
+    var password = req.body.password;
+
+    var hash = bcrypt.hashSync(password, 10);
+
+    var query_str = {
+        sql: 'INSERT INTO `users`(username, password) VALUES (?,?)',
+        values: [username, hash],
+        timeout: 2000
+    };
+
+    pool.getConnection(function (err, connection) {
+        if (err) {
+            throw err
+        }
+        connection.query(query_str, function (err, rows, fields) {
+            connection.release();
+            if (err) {
+                console.log(err);
+            }
+            res.status(200).json(rows);
+        });
+    });
+
 });
 
 //FALL BACK, DISPLAY SOME INFO
@@ -162,5 +244,6 @@ router.delete('/v1/cities/:ID?', function (req, res, next) {
         });
     });
 });
+
 
 module.exports = router;
