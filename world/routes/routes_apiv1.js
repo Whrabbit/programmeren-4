d5984744c573f044100c901ab57b00b0d63c3a4a
Whrabbit <whitneyrabbitc@gmail.com>
2017-05-31 18:56:28 +0200

    world

--- world/routes/routes_apiv1.js
+++ world/routes/routes_apiv1.js
@@ -1,48 +1,98 @@
-var express         = require('express');
-var router          = express.Router();
-var connector       = require('../db/db_connector');
-var pool            = require('../db/db_connector');
+var express = require('express');
+var router = express.Router();
+var connector = require('../db/db_connector');
+var pool = require('../db/db_connector');
+
+router.get('/login', function (req, res, next) {
+    var username = req.query.username || '';
+    var password = req.query.password || '';
+
+    if (username && password) {
+        var query_str = 'SELECT * FROM users WHERE username = "' + username + '" AND password = "' + password + '";';
+        console.log(query_str);
+        pool.getConnection(function (err, connection) {
+            if (err) {
+                throw err
+            }
+            connection.query(query_str, function (err, rows, fields) {
+                connection.release();
+                if (err) {
+                    throw err
+                }
+                res.status(200).json(rows);
+                if(rows.hasOwnProperty("username", username)){
+                    console.log("got em");
+                }
+            });
+        });
+    } else {
+       res.json({message: "Please provide a valid username and password"});
+    }
+});
+
+router.all(new RegExp("[^\/login)]"), function (req, res, next) {
+    var token = (req.header('X-Acces-Token')) || '';
+
+    auth.decodeToken(token, function (err, payload) {
+        if (err) {
+            console.log('Error handler: ' + err.message);
+            res.status((err.status || 401 )).json({error: new Error("Not authorised").message});
+        } else {
+            next();
+        }
+    });
+});
 
 //FALL BACK, DISPLAY SOME INFO
-router.get('/v1/cities/search', function(req, res, next){
+router.get('/v1/cities/search', function (req, res, next) {
     var countryCode = req.query.CountryCode;
     var limit = req.query.limit;
 
     var query_str = 'SELECT * FROM city WHERE CountryCode = "' + countryCode + '" LIMIT ' + limit + ';';
-    pool.getConnection( function(err, connection){
-        if (err){throw err}
-        connection.query(query_str, function(err, rows,fields){
+    pool.getConnection(function (err, connection) {
+        if (err) {
+            throw err
+        }
+        connection.query(query_str, function (err, rows, fields) {
             connection.release();
-            if(err){throw err}
+            if (err) {
+                throw err
+            }
             res.status(200).json(rows);
         });
     });
 });
-router.get('/v1/cities/:ID?', function(req, res, next){
+
+router.get('/v1/cities/:ID?', function (req, res, next) {
     var id = req.params.ID;
 
     var query_str;
-    if (id){
+    if (id) {
         query_str = 'SELECT * FROM city WHERE ID = "' + id + '";';
-    }else {
+    } else {
         query_str = 'SELECT * FROM city';
     }
-    pool.getConnection( function(err, connection){
-        if (err){throw err}
-        connection.query(query_str, function(err, rows,fields){
+    pool.getConnection(function (err, connection) {
+        if (err) {
+            throw err
+        }
+        connection.query(query_str, function (err, rows, fields) {
             connection.release();
-            if(err){throw err}
+            if (err) {
+                throw err
+            }
             res.status(200).json(rows);
         });
     });
 });
 
-router.post('/v1/cities', function (req, res, next){
+router.post('/v1/cities', function (req, res, next) {
     var ID = req.body.ID;
     var name = req.body.Name;
     var countryCode = req.body.CountryCode;
     var district = req.body.District;
     var population = req.body.Population;
+
 
     var query_str = {
         sql: 'INSERT INTO `city`(ID, name, countryCode, district, population) VALUES (?,?,?,?,?);',
@@ -51,51 +101,63 @@ router.post('/v1/cities', function (req, res, next){
     };
     console.log('query:' + query_str.sql);
     res.contentType('application/json');
-    pool.getConnection( function(err, connection){
-        if (err){throw err}
-        connection.query(query_str, function(err, rows,fields){
+    pool.getConnection(function (err, connection) {
+        if (err) {
+            throw err
+        }
+        connection.query(query_str, function (err, rows, fields) {
             connection.release();
-            if(err){throw err}
+            if (err) {
+                throw err
+            }
             res.status(200).json(rows);
         });
     });
 });
 
-router.put('/v1/cities/:ID?', function (req, res, next){
+router.put('/v1/cities/:ID?', function (req, res, next) {
     var city = req.body;
     var ID = req.params.ID;
 
     var query_str = {
-        sql: 'UPDATE `city` SET name = ?, district = ?, population = ? WHERE ID = '+ID,
-        values: [city.ID, city.name, city.countryCode, city.district, city.population],
+        sql: 'UPDATE `city` SET name = ?, district = ?, population = ? WHERE ID = ' + ID,
+        values: [city.name, city.district, city.population],
         timeout: 2000
     };
 
     console.log('query:' + query_str.sql);
     res.contentType('application/json');
-    pool.getConnection( function(err, connection){
-        if (err){throw err}
-        connection.query(query_str, function(err, rows,fields){
+    pool.getConnection(function (err, connection) {
+        if (err) {
+            throw err
+        }
+        connection.query(query_str, function (err, rows, fields) {
             connection.release();
-            if(err){throw err}
+            if (err) {
+                throw err
+            }
             res.status(200).json(rows);
         });
     });
 });
-router.delete('/v1/cities/:ID?', function(req, res, next){
+router.delete('/v1/cities/:ID?', function (req, res, next) {
     var id = req.params.ID;
 
     var query_str;
-    if (id){
+    if (id) {
         query_str = 'DELETE FROM city WHERE ID = "' + id + '";';
-    }else {
+    } else {
         query_str = 'SELECT * FROM city';
     }
-    pool.getConnection( function(err, connection){
-        if (err){throw err}
-        connection.query(query_str, function(err, rows,fields){
+    pool.getConnection(function (err, connection) {
+        if (err) {
+            throw err
+        }
+        connection.query(query_str, function (err, rows, fields) {
             connection.release();
-            if(err){throw err}
+            if (err) {
+                throw err
+            }
             res.status(200).json(rows);
         });
     });
